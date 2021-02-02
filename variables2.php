8f92c0fad2c494fe7fc4352f732e9ca731de13c2
SafaanHashmi <55549354+SafaanHashmi@users.noreply.github.com>
2021-02-02 18:08:18 +0530

    Add files via upload

---
+++ variables2.php
@@ -0,0 +1,9 @@
+<?php $a=1002; $b=3224; $c=2021; $d=4042; $e=2222; ?>
+<html>
+<head>
+<title> Variables 2 </title>
+</head>
+<body>
+<h1><?php echo $a+$b+$c+$d+$e;?></h1>
+</body>
+</html>
---
+++ variables3.php
@@ -0,0 +1,9 @@
+<?php $a=750000025; $b=60259826; ?>
+<html>
+<head>
+<title> Variables 2 </title>
+</head>
+<body>
+<h1><?php echo $a-$b;?></h1>
+</body>
+</html>
